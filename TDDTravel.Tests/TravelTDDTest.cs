72c120e373bcbddea6317b1409e3d859f4e321c1
alnicolaou96 <alnicolaou96@gmail.com>
2018-06-15 15:31:32 -0400

    Some Properties

---
+++ TDDTravel.Tests/TravelTDDTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDTravel.Tests
+{
+    class TravelTDDTest
+    {
+        [Test]
+        public void Default_CTR()
+        {
+            var trav = new TravelTDD("FName");
+            var result = trav.FirstName;
+            Assert.That(result, Is.EqualTo("FName"));
+        }
+        [Test]
+        public void Test_Lastname_Prop()
+        {
+            var trav = new TravelTDD(" ","LName");
+            var result = trav.LastName;
+            Assert.That(result, Is.EqualTo("LName"));
+        }
+        [Test]
+        public void Test_Address_Prop()
+        {
+            var trav = new TravelTDD("", "", "123 street name");
+            var result = trav.Address;
+            Assert.That(result, Is.EqualTo("123 street name"));
+        }
+        [Test]
+        public void Test_Email_Prop()
+        {
+            var trav = new TravelTDD("", "", "", "Email");
+            var result = trav.Email;
+            Assert.That(result, Is.EqualTo("Email"));
+        }
+
+    }
+}
---
+++ TDDTravel/TravelTDD.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDTravel
+{
+    public class TravelTDD
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public TravelTDD()
+        {
+            //default
+        }
+        public TravelTDD(string firstName)
+        {
+            this.FirstName = firstName;
+        }
+        public TravelTDD(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+        public TravelTDD(string firstName, string lastName, string address)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Address = address;
+        }
+        public TravelTDD(string firstName, string lastName, string address,string email)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Address = address;
+            this.Email = email;
+        }
+    }
+}
